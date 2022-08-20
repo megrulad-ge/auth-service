94ea8b2eaa8dd02e95f5ab4cac1b10fbc671dbbb
n1md7 <b.kodua@gmail.com>
2022-08-20 23:54:42 +0300

    chore: base strategies added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SetupModule } from './__common/setup/setup.module';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [SetupModule, HealthModule],
+  imports: [SetupModule, HealthModule, AuthModule, UsersModule],
+  controllers: [AppController],
 })
 export class AppModule {}
